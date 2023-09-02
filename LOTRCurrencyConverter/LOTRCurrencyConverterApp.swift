ee892940296a3f457914c09b13844680b8ab0fbb
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2023-09-02 08:51:24 -0400

    Initial Commit

---
+++ LOTRCurrencyConverter/LOTRCurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRCurrencyConverterApp.swift
+//  LOTRCurrencyConverter
+//
+//  Created by Jimmy Ghelani on 2023-09-02.
+//
+
+import SwiftUI
+
+@main
+struct LOTRCurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
